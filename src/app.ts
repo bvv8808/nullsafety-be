247790832269be69be894925c695f037a9d566b7
김현우 <kimhyeonwoo@Hyeonwoo-iMac.local>
2021-09-17 00:55:40 +0900

    init

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ const express = require("express");
 const app = express();
 
 app.set("port", process.env.PORT || 5000);
-
+// ?????
 app.use("/", require("./routes/rootRouter"));
 
 app.use((req: Request, res: Response, next: NextFunction) => {
