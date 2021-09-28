8c15c97e0c95a21618ff2d2698aaf5a2dfff9128
김현우 <kimhyeonwoo@Hyeonwoo-iMac.local>
2021-09-29 01:29:34 +0900

    modify start script

--- models/index.ts
+++ models/index.ts
@@ -4,25 +4,28 @@ const SequelizeConstructor = require("sequelize");
 const env = process.env.NODE_ENV || "development";
 const config = require("../config/config")[env];
 const db: any = {};
-const sequelize: Sequelize = new SequelizeConstructor(
-  config.database,
-  config.username,
-  config.password,
-  config
-);
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
+try {
+  const sequelize: Sequelize = new SequelizeConstructor(
+    config.database,
+    config.username,
+    config.password,
+    config
+  );
+  db.sequelize = sequelize;
+  db.Sequelize = Sequelize;
 
-db.Category = require("./category")(sequelize);
-db.Content = require("./content")(sequelize);
-db.Visitor = require("./visitor")(sequelize);
-db.Visit = require("./visit")(sequelize);
-db.Img = require("./img")(sequelize);
-db.Like = require("./like")(sequelize);
+  db.Category = require("./category")(sequelize);
+  db.Content = require("./content")(sequelize);
+  db.Visitor = require("./visitor")(sequelize);
+  db.Visit = require("./visit")(sequelize);
+  db.Img = require("./img")(sequelize);
+  db.Like = require("./like")(sequelize);
 
-db.Category.hasMany(db.Content, {
-  foreignKey: "cid",
-  sourceKey: "id",
-});
-
+  db.Category.hasMany(db.Content, {
+    foreignKey: "cid",
+    sourceKey: "id",
+  });
+} catch (e) {
+  console.warn("DB Error:::::: ", e);
+}
 module.exports = db;
