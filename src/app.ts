e21c9724a354f4233d3dafbefa9c7135eeaa8f9d
김현우 <kimhyeonwoo@Hyeonwoo-iMac.local>
2021-09-29 01:41:57 +0900

    modify start script

--- src/app.ts
+++ src/app.ts
@@ -6,8 +6,8 @@ dotenv.config();
 
 const app = express();
 
-// const { sequelize } = require("../models/");
-// sequelize.sync();
+const { sequelize } = require("../models/");
+sequelize.sync();
 
 app.set("port", process.env.PORT || 5000);
 app.use(express.urlencoded({ extended: true, limit: "20mb" }));
